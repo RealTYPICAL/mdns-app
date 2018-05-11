5c8c56141d6d42f18805d17125bb44a072212081
james <jetter019@msn.com>
2018-05-11 14:01:10 +0200

    Hello world

--- src/renderer.ts
+++ src/renderer.ts
@@ -1,3 +1,14 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+import * as bonjourModule from "bonjour";
+
+const bonjour = bonjourModule();
+
+bonjour.find({ type: "music-voting" }, (service: bonjourModule.Service) => {
+    console.log("Found an HTTP server: ", service);
+    // const address = _.find(service.addresses, e => ip.isV4Format(e));
+    // if (address) {
+    //     doAction(address, service.port);
+    // }
+});
